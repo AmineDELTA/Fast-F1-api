394422135316441e88bbeaececdbf7645d78627f
Ganar <am.elgueddar@gmail.com>
2025-07-14 01:25:09 +0100

    add two tests

--- rate_limit.py
+++ rate_limit.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import Request, HTTPException
 from datetime import datetime, timedelta
 from collections import defaultdict
 
--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,7 @@ from main import app, get_db
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database import Base
+from models import Driver, Team
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # update later on
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -32,3 +33,24 @@ def client(db_session):
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
     app.dependency_overrides.clear()
+
+@pytest.fixture
+def test_team(db_session):
+    team = Team(name="Mercedes", victories=125, championships=8)
+    db_session.add(team)
+    db_session.commit()
+    return team
+
+@pytest.fixture
+def test_driver(db_session, test_team):
+    driver = Driver(
+        first_name="Lewis",
+        last_name="Hamilton",
+        number=44,
+        age=38,
+        nationality="British",
+        team_name_id=test_team.id
+    )
+    db_session.add(driver)
+    db_session.commit()
+    return driver
